58085a0691cc10a82f53ed2b36e923fb562efa84
Bartlomiej Obudzinski <bobudzinski@amplifier.pl>
2023-03-04 12:08:07 +0100

    feat: initalize project, add typescript and absolute paths

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@Components/Atoms/Button';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function App() {
+
+
+
+  
+  return (
+    <SafeAreaView >
+      <Text>There will be Cardio Activity App!</Text>
+      <Button/>
+    </SafeAreaView>
+  );
+}
+
+
